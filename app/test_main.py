d5f99cf93beaacc699cb7904d9088f40c6a66800
Dark Round <kozanroman2007@gmail.com>
2026-09-08 17:16:48 +0300

    I hate flake8

--- app/test_main.py
+++ app/test_main.py
@@ -1,6 +1,7 @@
 import pytest
 from app.main import get_human_age
 
+
 @pytest.mark.parametrize("cat_age, dog_age, expected", [
     (0, 0, [0, 0]),
     (14, 14, [0, 0]),
@@ -11,5 +12,5 @@ from app.main import get_human_age
     (28, 28, [3, 2]),
     (100, 100, [21, 17]),
 ])
-def test_get_human_age(cat_age, dog_age, expected):
+def test_get_human_age(cat_age: int, dog_age: int, expected: list[int]) -> None:
     assert get_human_age(cat_age, dog_age) == expected
